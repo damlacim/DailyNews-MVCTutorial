24d9b717a5addfa59908883e6f184a7a1fdc79fd
damlacim <cim.damla@gmail.com>
2022-07-05 10:13:06 +0300

    Initial Commit

---
+++ DailyNews-MVCTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNews-MVCTutorial
+//
+//  Created by Damla Çim on 5.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
