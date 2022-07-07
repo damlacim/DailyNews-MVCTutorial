5815a13b9d875516930a56adbee841f40973fb77
damlacim <cim.damla@gmail.com>
2022-07-07 10:55:32 +0300

    add table view process and reload data to ui

--- DailyNews-MVCTutorial/Controller/ArticleViewController.swift
+++ DailyNews-MVCTutorial/Controller/ArticleViewController.swift
@@ -18,7 +18,8 @@ protocol ArticleViewControllerDelagate: AnyObject {
     func reloadTableView()
 }
 
-class ArticleViewController: UIViewController {
+class ArticleViewController: UIViewController, ArticleViewControllerDelagate {
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: Delegate
     weak var delegate: ArticleViewControllerDelagate?
@@ -26,16 +27,35 @@ class ArticleViewController: UIViewController {
     // MARK: Private variables
     private var articles: [Article]?
     private var viewStateBlock: ((ViewState) -> Void)?
+    private var selectIndex = 0
     
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        subscribeViewModel()
         fetchData()
+        reloadTableView()
+        
     }
     
     // MARK: Functions
     func listenViewModel(with completion: @escaping (ViewState) -> Void) {
         viewStateBlock = completion
+    }
+    
+    private func subscribeViewModel() {
+        listenViewModel { [weak self] state in
+            switch state {
+            case .loading:
+                print("loading")
+            case .done:
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }
     }
     
     private func fetchData() {
@@ -49,6 +69,13 @@ class ArticleViewController: UIViewController {
                 print(articles)
             }
             self.viewStateBlock?(.done)
+        }
+    }
+    
+    // MARK: Function
+    func reloadTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
         }
     }
 }
@@ -68,6 +95,40 @@ extension ArticleViewController {
         }
         return article
     }
+}
+
+extension ArticleViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.numberOfRowsInSection(section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell",for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        let articleIndex = self.articleIndex(indexPath.row)
+        cell.titleLabel.text = articleIndex.title
+        cell.descriptionLabel.text = articleIndex.description
+        return cell
+    }
+    
+  /*  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        selectIndex = indexPath.row
+        print(selectIndex)
+        performSegue(withIdentifier: "detail", sender: self)
+    }*/
+    
+  /* override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let detailViewController = segue.destination as? DetailViewController {
+            let articleVM = self.articleIndex(selectIndex)
+            detailViewController.articles = articleVM
+            print(articleVM)
+        }
+    }*/
+    
+    
 }
 
 
---
+++ DailyNews-MVCTutorial/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  DailyNews-MVCTutorial
+//
+//  Created by Damla Çim on 7.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    public var articles: Article?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
